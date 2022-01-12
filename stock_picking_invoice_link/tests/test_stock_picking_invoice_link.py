1402846c085714aa085d5bc09f2048ac3d585953
Antony Herrera <antonyht27@gmail.com>
2022-01-12 01:26:08 -0400

    [MIG] stock_picking_invoice_link: Migration to 15.0

--- stock_picking_invoice_link/tests/test_stock_picking_invoice_link.py
+++ stock_picking_invoice_link/tests/test_stock_picking_invoice_link.py
@@ -3,13 +3,13 @@
 # Copyright 2017 Jacques-Etienne Baudoux <je@bcim.be>
 # Copyright 2021 Tecnativa - João Marques
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 from odoo.exceptions import UserError
-from odoo.tests import Form
+from odoo.tests import Form, tagged
 
 from odoo.addons.sale.tests.common import TestSaleCommon
 
 
+@tagged("post_install", "-at_install")
 class TestStockPickingInvoiceLink(TestSaleCommon):
     def _update_product_qty(self, product):
 
@@ -247,7 +247,12 @@ class TestStockPickingInvoiceLink(TestSaleCommon):
         wiz_invoice_refund = (
             self.env["account.move.reversal"]
             .with_context(active_model="account.move", active_ids=inv.ids)
-            .create({"refund_method": "modify", "reason": "test"})
+            .create(
+                {
+                    "refund_method": "modify",
+                    "reason": "test",
+                }
+            )
         )
         wiz_invoice_refund.reverse_moves()
         new_invoice = self.so.invoice_ids.filtered(
