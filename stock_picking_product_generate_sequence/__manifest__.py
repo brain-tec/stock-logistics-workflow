6158e8d4fa5e0a325d789ada2f71da302ade9b27
David Montull <david.montull@bt-group.com>
2022-08-11 17:36:30 +0200

    [t130721] Fix pre-commit

--- stock_picking_product_generate_sequence/__manifest__.py
+++ stock_picking_product_generate_sequence/__manifest__.py
@@ -5,23 +5,23 @@
 # See LICENSE file for full licensing details.
 ##############################################################################
 {
-    'name': 'Stock Picking Product Generate sequence',
-    'version': '15.0.1.0.0',
-    'summary': 'Add an automated sequence in product of stock picking',
-    'category': 'Inventory',
-    'author': 'brain-tec AG, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/stock-logistics-workflow',
-    'depends': [
-        'product',
-        'stock',
+    "name": "Stock Picking Product Generate sequence",
+    "version": "15.0.1.0.0",
+    "summary": "Add an automated sequence in product of stock picking",
+    "category": "Inventory",
+    "author": "brain-tec AG, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "depends": [
+        "product",
+        "stock",
     ],
-    'license': 'AGPL-3',
-    'data': [
-        'views/product_template_views.xml',
-        'views/stock_move_line_views.xml',
-        'data/stock_picking_product_sequence.xml',
+    "license": "AGPL-3",
+    "data": [
+        "views/product_template_views.xml",
+        "views/stock_move_line_views.xml",
+        "data/stock_picking_product_sequence.xml",
     ],
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+    "installable": True,
+    "application": False,
+    "auto_install": False,
 }
--- stock_picking_product_generate_sequence/models/stock_move_line.py
+++ stock_picking_product_generate_sequence/models/stock_move_line.py
@@ -4,23 +4,22 @@
 #
 # See LICENSE file for full licensing details.
 ##############################################################################
-from re import findall as regex_findall
 from re import split as regex_split
 
-from odoo import models, api
+from odoo import api, models
 
 
 class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'
+    _inherit = "stock.move.line"
 
     def action_generate_product_serial(self):
         self.ensure_one()
-        self.lot_id = self.env['stock.production.lot'].create(
+        self.lot_id = self.env["stock.production.lot"].create(
             {
-                'product_id': self.product_id.id,
-                'company_id': self.company_id.id,
-                'name': self._get_next_serial(self.company_id, self.product_id)
-                or self.env['ir.sequence'].next_by_code('stock.picking.product'),
+                "product_id": self.product_id.id,
+                "company_id": self.company_id.id,
+                "name": self._get_next_serial(self.company_id, self.product_id)
+                or self.env["ir.sequence"].next_by_code("stock.picking.product"),
             }
         )
 
@@ -28,26 +27,26 @@ class StockMoveLine(models.Model):
     def _get_next_serial(self, company, product):
         sequence = product.product_tmpl_id.serial_lot
         if sequence:
-            splitted = regex_split(r'(\d+)', sequence)
+            splitted = regex_split(r"(\d+)", sequence)
             prefix = splitted[0]
             initial_number = int(splitted[1])
             padding = len(splitted[1])
-            last_serial = self.env['stock.production.lot'].search(
+            last_serial = self.env["stock.production.lot"].search(
                 [
-                    ('company_id', '=', company.id),
-                    ('product_id', '=', product.id),
-                    ('name', '=ilike', '%s%%' % prefix)
+                    ("company_id", "=", company.id),
+                    ("product_id", "=", product.id),
+                    ("name", "=ilike", "%s%%" % prefix)
                 ],
                 limit=1,
-                order='id DESC'
+                order="id DESC"
             )
 
             if last_serial:
-                last_serial_splitted = regex_split(r'(\d+)', last_serial.name)
+                last_serial_splitted = regex_split(r"(\d+)", last_serial.name)
                 last_serial_prefix = last_serial_splitted[0]
                 last_serial_number = int(last_serial_splitted[1])
-                # return last_serial_prefix + str(last_serial_number + 1).zfill(padding)
-                return "".join([last_serial_prefix, str(last_serial_number + 1).zfill(padding)])
-            # return prefix + str(initial_number + 1).zfill(padding)
+                return "".join(
+                    [last_serial_prefix, str(last_serial_number + 1).zfill(padding)]
+                )
             return "".join([prefix, str(initial_number + 1).zfill(padding)])
         return False
